f19d3714ab2c2fa50bbdc1a0d61d2aa3d6dc610c
AHUyzk <3027272627@qq.com>
2025-08-03 21:24:43 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-     
-python train.py --model PINN --device 'cuda:0'
-python train.py --model QRes --device 'cuda:0'
-python train.py --model FLS --device 'cuda:0'
-python train.py --model PINNsFormer --device 'cuda:0'
-python train.py --model PINNMamba --device 'cuda:0'
